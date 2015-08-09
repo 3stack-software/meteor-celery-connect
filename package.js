fdbff229ecbb3f4be3104c85dbe4dd3bc38d7132
Nathan Muir <ndmuir@gmail.com>
2015-08-09 13:37:14 +1000

    Bump: 3.0.0 -> 3.0.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: '3stack:celery-connect',
-  version: '3.0.0',
+  version: '3.0.1',
   summary: 'Simple interface to use 3stack:celery-shoot, just set environment variable CELERY_BROKER_URL',
   git: 'https://github.com/3stack-software/meteor-celery-connect',
   documentation: 'README.md'
@@ -10,7 +10,7 @@ Package.onUse(function(api){
   api.versionsFrom('METEOR@1.1.0.2');
 
   api.use('logging', 'server');
-  api.use('3stack:celery-shoot@4.0.1', 'server');
+  api.use('3stack:celery-shoot@4.0.3', 'server');
 
   api.export('Celery', 'server');
 
