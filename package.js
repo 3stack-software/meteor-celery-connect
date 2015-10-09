edbcd8f9e41831e659db4c09b535a5025e9101dd
Nathan Muir <ndmuir@gmail.com>
2015-10-09 14:55:14 +1000

    Release version 3.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: '3stack:celery-connect',
-  version: '3.0.1',
+  version: '3.1.0',
   summary: 'Simple interface to use 3stack:celery-shoot, just set environment variable CELERY_BROKER_URL',
   git: 'https://github.com/3stack-software/meteor-celery-connect',
   documentation: 'README.md'
